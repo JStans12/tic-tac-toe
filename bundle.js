ebe856f9f5933942027098c7e0a9e23c0bd193ca
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-11 14:28:25 -0700

    players can choose a name

--- bundle.js
+++ bundle.js
@@ -10352,7 +10352,9 @@ var $ = __webpack_require__(1);
 var socket = io();
 var Player = __webpack_require__(2);
 var Game = __webpack_require__(0);
+var player;
 var playerRole;
+var enemyName = 'sandy'
 
 socket.on('connectCount', function(connectCount){
   if(playerRole == undefined){
@@ -10365,12 +10367,37 @@ function setPlayerRole(role){
   var roles = {1: 'o player', 2: 'x player',};
   playerRole = roles[role] || 'spectating';
   $('#game-state').html(playerRole);
+  if(playerRole != 'spectating'){
+    $('#name-picker').removeClass('hidden');
+  }
+}
+
+function formatGameHeader(){
+  $('#game-state').html('');
+  var enemy = enemyName == undefined ? '...':enemyName;
+  if(player.marker == 'o'){
+    $('#game-state').html('o: ' + player.name + ' - x: ' + enemy + '');
+  } else {
+    $('#game-state').html('o: ' + enemy + ' - x: ' + player.name + '');
+  }
+  $('#name-picker').addClass('hidden');
 }
 
 $(document).ready(function(){
+  $('#name-picker').submit(function(){
+    var marker = playerRole[0];
+    var name = $('#player-name').val();
+    player = new Player(marker, name);
+    formatGameHeader(marker);
+  });
+
   $('td').click(function(){
     $(this).html('x');
   });
+
+  $('form').submit(function(e){
+    e.preventDefault();
+  })
 });
 
 
--- lobby.js
+++ lobby.js
@@ -2,7 +2,9 @@ var $ = require('jquery');
 var socket = io();
 var Player = require('./player');
 var Game = require('./game');
+var player;
 var playerRole;
+var enemyName;
 
 socket.on('connectCount', function(connectCount){
   if(playerRole == undefined){
@@ -15,10 +17,35 @@ function setPlayerRole(role){
   var roles = {1: 'o player', 2: 'x player',};
   playerRole = roles[role] || 'spectating';
   $('#game-state').html(playerRole);
+  if(playerRole != 'spectating'){
+    $('#name-picker').removeClass('hidden');
+  }
+}
+
+function formatGameHeader(){
+  $('#game-state').html('');
+  var enemy = enemyName == undefined ? '...':enemyName;
+  if(player.marker == 'o'){
+    $('#game-state').html('o: ' + player.name + ' - x: ' + enemy + '');
+  } else {
+    $('#game-state').html('o: ' + enemy + ' - x: ' + player.name + '');
+  }
+  $('#name-picker').addClass('hidden');
 }
 
 $(document).ready(function(){
+  $('#name-picker').submit(function(){
+    var marker = playerRole[0];
+    var name = $('#player-name').val();
+    player = new Player(marker, name);
+    formatGameHeader(marker);
+  });
+
   $('td').click(function(){
     $(this).html('x');
   });
+
+  $('form').submit(function(e){
+    e.preventDefault();
+  })
 });
