ff0b75390a26755f80995664f7c545874a6c3c03
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-11 12:31:42 -0700

    games can check for win condition

--- game.js
+++ game.js
@@ -2,4 +2,32 @@ var Game = function(boardState){
   this.boardState = boardState;
 }
 
+Game.prototype.checkForWinner = function(){
+  var state = this.boardState
+  if (hasWon('o', state)) {
+    return "o";
+  } else if (hasWon('x', state)) {
+    return "x";
+  } else {
+    return "none";
+  }
+}
+
+function hasWon(marker, state){
+  if (
+    (state[0] == marker && state[1] == marker && state[2] == marker) ||
+    (state[3] == marker && state[4] == marker && state[5] == marker) ||
+    (state[6] == marker && state[7] == marker && state[8] == marker) ||
+    (state[0] == marker && state[3] == marker && state[6] == marker) ||
+    (state[1] == marker && state[4] == marker && state[7] == marker) ||
+    (state[2] == marker && state[5] == marker && state[8] == marker) ||
+    (state[0] == marker && state[4] == marker && state[8] == marker) ||
+    (state[6] == marker && state[4] == marker && state[2] == marker)
+  ){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 module.exports = Game;
--- test/game_test.js
+++ test/game_test.js
@@ -7,3 +7,75 @@ describe("games have a state", function(){
     assert.deepEqual(game.boardState, ['o','x','o','x','o','x','n','n','n']);
   });
 });
+
+describe("games can find winner first condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','o','o','x','n','x','x','n','n']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "o");
+  });
+});
+
+describe("games can find winner second condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','x','o','x','x','x','o','o','n']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "x");
+  });
+});
+
+describe("games can find winner third condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','n','o','x','o','o','x','x','x']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "x");
+  });
+});
+
+describe("games can find winner fourth condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','n','o','o','x','n','o','x','x']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "o");
+  });
+});
+
+describe("games can find winner fifth condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','x','o','o','x','n','n','x','n']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "x");
+  });
+});
+
+describe("games can find winner sixth condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','x','o','x','x','o','n','n','o']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "o");
+  });
+});
+
+describe("games can find winner seventh condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','n','n','n','o','n','n','n','o']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "o");
+  });
+});
+
+describe("games can find winner eigth condition", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','n','x','n','x','n','x','n','o']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "x");
+  });
+});
+
+describe("games can find no winner", function(){
+  it("returns the winner", function(){
+    var game = new Game(['o','n','x','n','n','n','x','n','o']);
+    var winner = game.checkForWinner();
+    assert.equal(winner, "none");
+  });
+});
